75615fb74728f2a67ed2c7cbba26794773c1c313
Halley Tsai <halley.tsai@idexbiometrics.com>
2020-04-14 12:32:39 -0400

    experimental recipe: correct paths in tools

--- tools/Install.bat
+++ tools/Install.bat
@@ -1,4 +1,7 @@
 del %AppData%\Rime\*.* /Q
 rmdir %AppData%\RIme\opencc /S /Q
-xcopy .\Rime\*.* /S %AppData%\Rime\
+xcopy ..\*.yaml /S %AppData%\Rime\
+xcopy ..\*.lua  /S %AppData%\Rime\
+xcopy ..\opencc /S %AppData%\Rime\
+xcopy ..\default.custom.in %AppData%\Rime\default.custom.yaml
 "C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" /deploy
